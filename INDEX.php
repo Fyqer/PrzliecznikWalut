b019b1bc471928fbc3e0ada25029a18ce20a1a77
Fyqer <82710043+Fyqer@users.noreply.github.com>
2021-04-27 12:15:33 +0200

    Add files via upload

---
+++ INDEX.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE HTML>
+<html>
+<?php
+#deklaracje
+$Imie = "Daniel"; 
+$Nazwisko = "Gałeczka";
+$Szkola =  "Wyższa Szkoła Technologii Informatycznych w Katowicach";
+?>
+<head>
+<title> Przelicznik walut</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+<div id = "main">
+<h1 >Przelicznik walut</h1>
+<div id = "TRESC">
+</br>
+
+</br>
+</br>
+</br>
+</br>
+<form action = "wynik.php" method="POST">
+<select name="wybor">
+<option value="GBP">GBP</option>
+<option value="EUR">EUR</option>
+
+<option value="USD">USD</option>
+
+</select>
+</br>
+</br>
+</br>
+</br>
+	<input type =number name= "PLN" placeholder="Kwota w PLN">
+</br>
+</br>
+
+<input type = submit value = "Przelicz!" style = "font-size:40px" />
+	
+	</div>
+	<div id = "STOPKA">
+	<p> <?php echo " Wykonane przez $Imie $Nazwisko - $Szkola";?> </p>
+	</div>
+</div>
+</form>
+</body>
+</html>
---
+++ gra.php
@@ -0,0 +1,75 @@
+﻿<?php
+   include('session.php');
+   
+  $nazwa = $login_session;
+  $state = 1;
+?>
+<html">
+   
+   <head>
+      <title>TodoAPP </title>
+   </head>
+   <body>
+   
+      <h1 align = center>Witaj <?php echo $nazwa; ?></h1> 
+	      <h2 align = left ><a href = "logout.php">Wyloguj </a></h2>
+		  <?php
+		  // wyswietlaanie zadań aktualnych
+		  
+		   
+		   while($wiersz = $wynik->fetch_assoc()){
+			   
+			     echo "<p><b> | Zadanie </b> : " .$wiersz['tytul'];
+		  
+		   echo "</br> | <b> |Opis </b> : ".$wiersz['opis']."</p>";
+		   }
+		   
+		   
+		   //DODAWANIE REKORDU 
+		   
+		   
+
+
+ if(!empty($_POST['tytul'])) // BLAD - PO odswiezeniu dodaje nowe zadanie 
+		   {
+		   $tytul = htmlentities('tytul', ENT_QUOTES, "UTF-8");
+       $opis = htmlentities('opis', ENT_QUOTES, "UTF-8");
+		   $nazwa = $login_session;
+  $state = 1;
+		   $tytul = mysqli_real_escape_string($db, $_POST['tytul']);
+$opis = mysqli_real_escape_string($db, $_POST['opis']);
+
+$sqladd = "INSERT INTO tasks (tytul, opis, state, login ) VALUES ('$tytul', '$opis', '$state', '$nazwa' )";
+if(mysqli_query($db, $sqladd)){
+    echo "Rekord pomylsnie dodany.";
+} else{
+   echo "ERROR";
+}
+ 
+ $tytul = "";
+// Close connection
+mysqli_close($db);
+		   }
+		   ?>
+		
+		   
+	 </br>
+		   
+     <div align = center>
+	 <p><b>Dodaj nowe zadanie <b></p> 
+	 <form action="gra.php" method="post">
+    <p>
+        <label for="tytul">Tytuł :</label>
+        <input type="text" name="tytul" id="tytul">
+    </p>
+    <p>
+        <label for="opis">Opis:</label>
+        <input type="text" name="opis" id="opis">
+    </p>
+    
+    <input type="submit" name = "Dodaj" value = "Dodaj">
+</form>
+	 </div>
+   </body>
+   
+</html>
